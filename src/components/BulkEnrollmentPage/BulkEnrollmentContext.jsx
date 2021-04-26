a751fc495fbae364bdc032ca10a2192f99972395
Adrienne Bolger <abolger@edx.org>
2021-04-26 15:58:24 -0400

    TEMP STOPPING POINT, SHARING FOR LBIRCH

--- src/components/BulkEnrollmentPage/BulkEnrollmentContext.jsx
+++ src/components/BulkEnrollmentPage/BulkEnrollmentContext.jsx
@@ -1,6 +1,25 @@
 import React, { createContext, useState } from 'react';
 import PropTypes from 'prop-types';
 
+const REMOVE_ITEM_ACTION = 'REMOVE_ITEM_ACTION';
+export const removeAction = (itemId) => ({
+  type: REMOVE_ITEM_ACTION,
+  itemId
+});
+
+const reducer = (rowListState = [], action) => {
+  switch (action.type) {
+    case REMOVE_ITEM_ACTION:
+      return rowListState.filter(item => item.id != action.itemId);
+    default:
+      return rowListState;
+  }
+
+}
+
+//TODO: dispatch + action to add items, 
+// 
+
 export const BulkEnrollContext = createContext({});
 
 const BulkEnrollContextProvider = ({ children }) => {
--- src/components/BulkEnrollmentPage/stepper/ReviewStep.jsx
+++ src/components/BulkEnrollmentPage/stepper/ReviewStep.jsx
@@ -1,22 +1,41 @@
 import React, { useContext } from 'react';
 // import PropTypes from 'prop-types';
-
-import { BulkEnrollContext } from '../BulkEnrollmentContext';
+import {
+  Card, Pagination, Table, IconButton, Icon
+} from '@edx/paragon';
+import { Delete } from '@edx/paragon/icons';
+import { BulkEnrollContext, removeAction } from '../BulkEnrollmentContext';
 import { REVIEW_TITLE } from './constants';
+
+
+const EnrollmentListItem = ({ text, onClick }) => {
+  return (
+    <li><Card style={{}}>
+      <Card.Body className='d-flex justify-content-between'>
+        <Card.Text>
+          {text}
+        </Card.Text>
+        <IconButton src={Delete} iconAs={Icon} alt='Delete' onClick={onClick} variant="primary" />
+
+      </Card.Body>
+    </Card>
+    </li>
+  )
+}
 
 const ReviewStep = () => {
   const { emails: [selectedEmails], courses: [selectedCourses] } = useContext(BulkEnrollContext);
-
+  console.log(selectedCourses)
   return (
     <>
       <h2>{REVIEW_TITLE}</h2>
       <h3>Emails</h3>
       <ul>
-        {selectedEmails.map((email) => <li>{email}</li>)}
+        {selectedEmails.map((email) => <EnrollmentListItem key={email} text={email} onClick={dispatch(removeAction())} />)}
       </ul>
       <h3>Courses</h3>
       <ul>
-        {selectedCourses.map((course) => <li>{course.title}</li>)}
+        {selectedCourses.map((course) => <EnrollmentListItem key={course.key} text={course.title} onClick={dispatch(removeAction())} />)}
       </ul>
     </>
   );
